84971d0132af34eb8ef17eff1628e61c3048d767
Muhammad Yamin <muhammadyamin1081@gmail.com>
2020-08-16 15:09:54 +0700

    Cetak Excel

--- application/views/cetak/cetak_keahlian_excel.php
+++ application/views/cetak/cetak_keahlian_excel.php
@@ -4,17 +4,35 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $excel = new PHPExcel();
 
 $excel->setActiveSheetIndex(0);
+$excel->getActiveSheet()->getColumnDimension('A')->setWidth(6);
+$excel->getActiveSheet()->getColumnDimension('B')->setWidth(24);
+$excel->getActiveSheet()->getColumnDimension('C')->setWidth(40);
+$excel->getActiveSheet()
+    ->mergeCells('A1:C1')
+    ->setCellValue('A1','Data Kompetensi Keahlian')
+    ->getStyle('A1')
+    ->applyFromArray(
+        array(
+            'font'=>array(
+                'size'=>20
+            ),
+            'alignment'=>array(
+                'horizontal'=>PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+                'vertical'=>PHPExcel_Style_Alignment::VERTICAL_CENTER
+            )
+        )
+    );
 
 $kolom_tabel = array("No","ID Kompetensi Keahlian","Nama Kompetensi Keahlian");
 
 $kolom = 0;
 
 foreach ($kolom_tabel as $field){
-    $excel->getActiveSheet()->setCellValueByColumnAndRow($kolom, 1, $field);
+    $excel->getActiveSheet()->setCellValueByColumnAndRow($kolom, 3, $field);
     $kolom++;
 }
 
-$baris_excel = 2;
+$baris_excel = 4;
 $no = 1;
 
 foreach ($query as $q){
@@ -24,6 +42,42 @@ foreach ($query as $q){
     $no++;
     $baris_excel++;
 }
+
+$excel->getActiveSheet()->getStyle('A3:C3')->applyFromArray(
+    array(
+        'font'=>array(
+            'bold'=>true
+        ),
+        'alignment'=>array(
+            'horizontal'=>PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+            'vertical'=>PHPExcel_Style_Alignment::VERTICAL_CENTER
+        ),
+        'borders'=>array(
+            'allborders'=>array(
+                'style'=>PHPExcel_Style_Border::BORDER_THIN
+            )
+        )
+    )
+);
+
+$excel->getActiveSheet()->getStyle('A4:C'.($baris_excel-1))->applyFromArray(
+    array(
+        'borders'=>array(
+            'allborders'=>array(
+                'style'=>PHPExcel_Style_Border::BORDER_THIN
+            )
+        )
+    )
+);
+
+$excel->getActiveSheet()->getStyle('A4:B'.($baris_excel))->applyFromArray(
+    array(
+        'alignment'=>array(
+            'horizontal'=>PHPExcel_Style_Alignment::HORIZONTAL_CENTER,
+            'vertical'=>PHPExcel_Style_Alignment::VERTICAL_CENTER
+        )
+    )
+);
 
 $excel_writer = PHPExcel_IOFactory::createWriter($excel, 'Excel2007');
 header('Content-Type: application/vnd.ms-excel');
